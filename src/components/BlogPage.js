fd92bc6987671f38fcb6c04429386ff0cd190778
shino <106672783+shino-shino@users.noreply.github.com>
2023-07-15 10:38:40 +0900

    fix：ブログページにheadタグ&imgを追加

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import profileImage from "../Images/sibainu.jpg";
 
 const BlogPage = () => {
   return (
+    <>
+    <head>
+      <title>ブログページ</title>
+      <meta
+      name="description"
+      content="Blog Page : Web site created using create-react-app"
+      />
+    </head>
     <div className='container'>
       <h1>ブログ</h1>
+      <img src={profileImage} className="profileImage" />
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
@@ -17,6 +27,7 @@ const BlogPage = () => {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
     </div>
+    </>
   )
 }
 
