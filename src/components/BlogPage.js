904871ef2ba725b79c376b687099150ac1e8ce7c
shino <106672783+shino-shino@users.noreply.github.com>
2023-07-15 11:04:03 +0900

    fix：headタグ warningを修正

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -4,16 +4,17 @@ import profileImage from "../Images/sibainu.jpg";
 const BlogPage = () => {
   return (
     <>
-    <head>
+    {/* <head>
       <title>ブログページ</title>
       <meta
       name="description"
       content="Blog Page : Web site created using create-react-app"
       />
-    </head>
+    </head> */}
     <div className='container'>
+      <title>ブログページ</title>
       <h1>ブログ</h1>
-      <img src={profileImage} className="profileImage" />
+      <img src={profileImage} className="profileImage" alt='' />
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
